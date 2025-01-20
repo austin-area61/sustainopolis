6cba881b48951f88891043ab32f0984475ad1327
austin-area61 <austinonyango.area61@gmail.com>
2025-01-20 11:24:59 +0300

    used correct tailwind syntax

--- sustainopolis.0/components/ThemeToggle.tsx
+++ sustainopolis.0/components/ThemeToggle.tsx
@@ -1,17 +1,27 @@
-// ThemeToggle Component
+"use client";
+
 import React from "react";
-import { useTheme } from "@/contexts/ThemeContext";
+import { useTheme } from "../contexts/ThemeContext";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const ThemeToggle: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
+    <Button
+      variant="ghost"
+      size="icon"
       onClick={toggleTheme}
-      className="p-2 border rounded-lg focus:outline-none"
+      className="w-9 px-0"
     >
-      {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
-    </button>
+      {theme === "light" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 };
 
