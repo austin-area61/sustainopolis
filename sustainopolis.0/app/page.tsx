11def43b3745d147b6430bb2e01197a957c89ae6
austin-area61 <austinonyango.area61@gmail.com>
2025-01-20 11:34:29 +0300

    corrected client side rendering

--- sustainopolis.0/app/page.tsx
+++ sustainopolis.0/app/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import App from "@/components/App";
+const App = dynamic(() => import("@/components/App"), { ssr: false });
 
 export default function Home() {
   return <App />;
